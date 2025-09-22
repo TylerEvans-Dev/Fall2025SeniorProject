f1b61e692b1c6aeb9135bb1d29eee79c5cc3473a
Tyler Evans <evstye@gmail.com>
2025-09-22 16:47:10 -0600

    PWM redone need to be tested

--- CodeRobot/src/main.c
+++ CodeRobot/src/main.c
@@ -49,35 +49,52 @@ void memd(int* stepCount, int pinA, int pinB){
 #define PWM4_PIN 9
 
 #define MAX_RANGE_PWM 100
+#define PWM_RANGE 1024
+#define PWM_DIV 192
 
 void setupPwm(){
-    //belong to motor 1
-    softPwmCreate(PWM1_PIN, 0, MAX_RANGE_PWM); //red pin
-    softPwmCreate(PWM2_PIN, 0, MAX_RANGE_PWM); //black pin
-    //belong to motor 2
-    softPwmCreate(PWM3_PIN, 0, MAX_RANGE_PWM);//red pin
-    softPwmCreate(PWM4_PIN, 0, MAX_RANGE_PWM); //black pin
+    //setup the pinmode
+    pinMode(PWM1_PIN, PWM_OUTPUT);
+    pinMode(PWM2_PIN, PWM_OUTPUT);
+    pinMode(PWM3_PIN, PWM_OUTPUT);
+    pinMode(PWM4_PIN, PWM_OUTPUT);
+    //setup the PWM mode PWM_MODE_MS
+    pwmSetMode(PWM1_PIN, PWM_MODE_MS); // PWM 1
+    pwmSetMode(PWM2_PIN, PWM_MODE_MS); // PWM 2
+    pwmSetMode(PWM3_PIN, PWM_MODE_MS); // PWM 3
+    pwmSetMode(PWM4_PIN, PWM_MODE_MS); // PWM 4
+    //set clock speed.
+    pwmSetRange(PWM1_PIN, PWM_RANGE); //resolution PWM 1
+    pwmSetRange(PWM2_PIN, PWM_RANGE); //resolution PWM 2
+    pwmSetRange(PWM3_PIN, PWM_RANGE); //resolution PWM 3
+    pwmSetRange(PWM4_PIN, PWM_RANGE); //resolution PWM 4
+
+    pwmSetClock(PWM1_PIN, PWM_DIV); //div PWM 1
+    pwmSetClock(PWM2_PIN, PWM_DIV); //div PWM 2
+    pwmSetClock(PWM3_PIN, PWM_DIV); //div PWM 3
+    pwmSetClock(PWM4_PIN, PWM_DIV); //div PWM 4
 }
 
 void stop(){
     //must stop all to so set to zero.
-    softPwmWrite(PWM1_PIN, 0);
-    softPwmWrite(PWM2_PIN, 0);
-    softPwmWrite(PWM3_PIN, 0);
-    softPwmWrite(PWM4_PIN, 0);
+    pwmWrite(PWM1_PIN, 0);
+    pwmWrite(PWM2_PIN, 0);
+    pwmWrite(PWM3_PIN, 0);
+    pwmWrite(PWM4_PIN, 0);
     usleep(10); //added delay to help motor slow down
 }
 //every action made must be put at rest or it will damage the motor.
 void forward(int PWMval){
     stop(); // must be done to help motors be good
-    softPwmWrite(PWM3_PIN, PWMval);
-    softPwmWrite(PWM1_PIN, PWMval);
+    pwmWrite(PWM3_PIN, PWMval);
+    pwmWrite(PWM1_PIN, PWMval);
+
 }
 
 void backward(int PWMval){
     stop(); //must be done to help motor health
-    softPwmWrite(PWM4_PIN, PWMval);
-    softPwmWrite(PWM2_PIN, PWMval);
+    pwmWrite(PWM4_PIN, PWMval);
+    pwmWrite(PWM2_PIN, PWMval);
 }
 
 void turn(float deg){
