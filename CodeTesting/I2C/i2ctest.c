9404da1b3e113672b672149f574e4d7190fa6a8d
Tyler Evans <evstye@gmail.com>
2025-10-07 21:02:17 -0600

    attempt 2 i2c

--- CodeTesting/I2C/i2ctest.c
+++ CodeTesting/I2C/i2ctest.c
@@ -6,64 +6,51 @@
 
 #define VL53L0X_ADDR 0x29
 
-// ---------- Basic I2C helpers ----------
-void i2c_write_byte(int fd, uint8_t reg, uint8_t val) {
-    wiringPiI2CWriteReg8(fd, reg, val);
-}
-
-uint8_t i2c_read_byte(int fd, uint8_t reg) {
+uint8_t read_byte(int fd, uint8_t reg) {
     return wiringPiI2CReadReg8(fd, reg);
 }
 
-uint16_t i2c_read_word(int fd, uint8_t reg) {
+void write_byte(int fd, uint8_t reg, uint8_t val) {
+    wiringPiI2CWriteReg8(fd, reg, val);
+}
+
+uint16_t read_word(int fd, uint8_t reg) {
     int hi = wiringPiI2CReadReg8(fd, reg);
     int lo = wiringPiI2CReadReg8(fd, reg + 1);
     return (hi << 8) | lo;
 }
 
-// ---------- Wait for measurement completion ----------
-void wait_for_measurement(int fd) {
-    uint8_t status;
-    do {
-        status = i2c_read_byte(fd, 0x13);
-        delay(10);
-    } while ((status & 0x07) == 0);
-}
-
-// ---------- VL53L0X minimal init ----------
 void vl53l0x_init(int fd) {
-    // Power-up and boot sequence
-    i2c_write_byte(fd, 0x88, 0x00);
-    i2c_write_byte(fd, 0x80, 0x01);
-    i2c_write_byte(fd, 0xFF, 0x01);
-    i2c_write_byte(fd, 0x00, 0x00);
-    i2c_write_byte(fd, 0x91, 0x3C);
-    i2c_write_byte(fd, 0x00, 0x01);
-    i2c_write_byte(fd, 0xFF, 0x00);
-    i2c_write_byte(fd, 0x80, 0x00);
+    delay(10);
+    write_byte(fd, 0x88, 0x00);
+    write_byte(fd, 0x80, 0x01);
+    write_byte(fd, 0xFF, 0x01);
+    write_byte(fd, 0x00, 0x00);
+    write_byte(fd, 0x91, 0x3C);
+    write_byte(fd, 0x00, 0x01);
+    write_byte(fd, 0xFF, 0x00);
+    write_byte(fd, 0x80, 0x00);
 
-    // Minimal SPAD & sequence config
-    i2c_write_byte(fd, 0x01, 0x02);
+    // System sequence config
+    write_byte(fd, 0x01, 0x02);
+
+    // Start continuous ranging
+    write_byte(fd, 0x80, 0x01);
+    write_byte(fd, 0xFF, 0x01);
+    write_byte(fd, 0x00, 0x00);
+    write_byte(fd, 0x91, 0x3C);
+    write_byte(fd, 0x00, 0x01);
+    write_byte(fd, 0xFF, 0x00);
+    write_byte(fd, 0x80, 0x00);
+
+    write_byte(fd, 0x00, 0x04); // start continuous mode
 }
 
-// ---------- Take one distance reading ----------
 uint16_t vl53l0x_read_distance(int fd) {
-    // Start single measurement
-    i2c_write_byte(fd, 0x00, 0x01);
-
-    // Wait until data ready
-    wait_for_measurement(fd);
-
-    // Read distance result (register 0x1E or 0x14 depending on revision)
-    uint16_t dist = i2c_read_word(fd, 0x1E);
-
-    // Clear interrupt
-    i2c_write_byte(fd, 0x0B, 0x01);
-
+    uint16_t dist = read_word(fd, 0x14);
     return dist;
 }
 
-// ---------- Main ----------
 int main(void) {
     printf("VL53L0X Test (WiringOP)\n");
 
@@ -83,4 +70,3 @@ int main(void) {
 
     return 0;
 }
-
