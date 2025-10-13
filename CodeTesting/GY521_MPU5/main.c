899121da17b9924df4b76ec017a1e8d5d0e956ae
Tyler Evans <evstye@gmail.com>
2025-10-12 20:51:22 -0600

    added while loop

--- CodeTesting/GY521_MPU5/main.c
+++ CodeTesting/GY521_MPU5/main.c
@@ -16,9 +16,11 @@ int main(){
         return -1;
     }
     int suc = initGY521("/dev/i2c-4");
+    while(1){
     int readsuc = mpu6050_read_all(fd, &d);
     printf("A: %.2f %.2f %.2f | G: %.2f %.2f %.2f | T: %.2f°C\n",
                    d.ax, d.ay, d.az, d.gx, d.gy, d.gz, d.temp);
     delay(20);
+    }
     return 0;
 }
