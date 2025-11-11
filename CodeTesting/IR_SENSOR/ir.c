93dc78d9a09a383c1812e3d7cf72fc2a9d974707
Tyler Evans <evstye@gmail.com>
2025-11-10 20:42:10 -0700

    tunned the parameters -t

--- CodeTesting/IR_SENSOR/ir.c
+++ CodeTesting/IR_SENSOR/ir.c
@@ -14,7 +14,7 @@ int readPinIR(int PIN){
     int val = digitalRead(PIN);
     printf("GGGGGG the value red is %i \n ", val);
     //returns 1 if on surface pin will be high
-    if(val){
+    if(!val){
         return 1;
     }
     //returns 0 if on edge pin will be held low
@@ -25,9 +25,9 @@ int readPinIR(int PIN){
 
 int readIRObj(int PIN){
     int val = digitalRead(PIN);
-    //returns 1 if object present pin will be high
-    if(val){
-        return 1;
+    //returns -1 if object present pin will be high
+    if(!val){
+        return -1;
     }
     //returns 0 if object not present pin will be LOW
     else{
